4b9b2079b6596a4b1628a0cb79fb0f15b61d25a6
advanc3dUA <fash1k@gmail.com>
2023-03-13 11:36:07 +0100

    Rework of VC + haptic engine

--- DetectChanges/ViewController.swift
+++ DetectChanges/ViewController.swift
@@ -13,31 +13,32 @@ class ViewController: UIViewController {
     var soundManager = SoundManager()
     let networkManager = NetworkManager()
     let consolePrinter = ConsolePrinter()
+    let feedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
     var isFirstRun = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+  
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         Timer.scheduledTimer(withTimeInterval: 30, repeats: true) {[unowned self] timer in
             networkManager.start { apartments in
                 DispatchQueue.main.async { [unowned self] in
-                    if apartments.count > 0 {
+                    apartments.forEach { apartment in
+                        consoleTextView.text += consolePrinter.foundNew(apartment)
+                    }
+                    if !isFirstRun {
                         apartments.forEach { apartment in
-                            consoleTextView.text += consolePrinter.foundNew(apartment)
+                            soundManager.playAlert()
+                            feedbackGenerator.impactOccurred()
+                            guard let immomioLink = apartment.immomioLink, let url = URL(string: immomioLink) else { return }
+                            UIApplication.shared.open(url)
                         }
-                        if !isFirstRun {
-                            apartments.forEach { apartment in
-                                consoleTextView.text += consolePrinter.foundNew(apartment)
-                                soundManager.playAlert()
-                                guard let immomioLink = apartment.immomioLink, let url = URL(string: immomioLink) else { return }
-                                UIApplication.shared.open(url)
-                            }
-                        }
-                    } else {
+                    }
+                    
+                    if apartments.isEmpty {
                         consoleTextView.text += consolePrinter.notFound()
                     }
                     isFirstRun = false
