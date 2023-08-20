7b879bbc0d9038bfe1e11d0f1e4700366afcec32
advanc3dUA <fash1k@gmail.com>
2023-08-20 17:24:46 +0200

    Refactor VonoviaJson: internal structure VonoviaApartment moved to extension

--- WohnungSuchen/Models/VonoviaJson.swift
+++ WohnungSuchen/Models/VonoviaJson.swift
@@ -15,14 +15,15 @@ struct VonoviaJson: Decodable {
     }
 }
 
-struct VonoviaApartment: Decodable {
-    let wrk_id: String
-    let strasse: String
-    let titel: String
-    let preis: String
-    let groesse: String
-    let anzahl_zimmer: String
-    let slug: String
+extension VonoviaJson {
+    struct VonoviaApartment: Decodable {
+        let wrk_id: String
+        let strasse: String
+        let titel: String
+        let preis: String
+        let groesse: String
+        let anzahl_zimmer: String
+        let slug: String
+    }
 }
-
 
