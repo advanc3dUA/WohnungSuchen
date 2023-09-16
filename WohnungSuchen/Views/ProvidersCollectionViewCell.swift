326035a4d5958a8bf50583e182d19eb9ca0ce209
advanc3dUA <fash1k@gmail.com>
2023-09-16 15:03:35 +0200

    Add missing final to WarningAlertControllFactory class

--- WohnungSuchen/Views/ProvidersCollectionViewCell.swift
+++ WohnungSuchen/Views/ProvidersCollectionViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProvidersCollectionViewCell: UICollectionViewCell {
+final class ProvidersCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet var providerButton: SaveButton!
 
--- WohnungSuchen/WarningAlertControllerFactory.swift
+++ WohnungSuchen/WarningAlertControllerFactory.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class WarningAlertControllerFactory {
+final class WarningAlertControllerFactory {
     func getDescription(for error: AppError) -> String {
         var description = ""
         switch error {
         case .networkManagerError(.dataUnavailable): description = "Network Manager error: data is unavailable."
         case .networkManagerError(.server(statusCode: let code)): description = "Server error: status code = \(code)."
-        case .networkManagerError(.transport(let code)): description = "Transport error. Reason: \(code.localizedDescription)"
+        case .networkManagerError(.transport(let code)): description = "Transport error: \(code.localizedDescription)"
         case .immomioLinkError(.docCreationFailed): description = "ImmomioLink fetcher error: doc creation failed."
         case .immomioLinkError(.linkExtractionFailed): description = "ImmomioLink fetcher error: couldn't extract link."
         case .landlordError(.sagaDocCreationFailed): description = "Landlords error: Saga doc creation failed."
